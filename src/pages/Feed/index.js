7bace791f1a2cd04224c68e4cdd7c8909d9df9ac
Guilherme de Abreu Ramos <Gui@MacBook-Pro.local>
2019-09-05 10:26:44 +0900

    Add: file structure; API call; UI render; Pagination

---
+++ src/pages/Feed/index.js
@@ -0,0 +1,53 @@
+import React, {useState, useEffect} from 'react';
+import {View, FlatList} from 'react-native';
+
+import {Post, Header, Avatar, Name, PostImage, Description} from './styles';
+
+export default function Feed() {
+  const [feed, setFeed] = useState([]);
+  const [page, setPage] = useState(1);
+  const [total, setTotal] = useState(0);
+
+  async function loadPage(pageNumber = page) {
+    if (total && pageNumber > total) return;
+    console.log(page);
+
+    const response = await fetch(
+      `http://localhost:3000/feed?_expand=author&_limit=5&_page=${pageNumber}`,
+    );
+
+    const data = await response.json();
+    const totalItems = response.headers.get('X-Total-Count');
+
+    setTotal(Math.floor(totalItems / 5));
+    setFeed([...feed, ...data]);
+    setPage(pageNumber + 1);
+  }
+
+  useEffect(() => {
+    loadPage();
+  }, []);
+
+  return (
+    <View>
+      <FlatList
+        data={feed}
+        keyExtractor={post => String(post.id)}
+        onEndReached={() => loadPage()}
+        onEndReachedThreshold={0.1}
+        renderItem={({item}) => (
+          <Post>
+            <Header>
+              <Avatar source={{uri: item.author.avatar}} />
+              <Name>{item.author.name}</Name>
+            </Header>
+            <PostImage ratio={item.aspectRatio} source={{uri: item.image}} />
+            <Description>
+              <Name>{item.author.name}</Name> {item.description}
+            </Description>
+          </Post>
+        )}
+      />
+    </View>
+  );
+}
